ae18dad807aff573893ae4db2955997475c6f0b5
Javier Revilla <jrevilla.pcm@gmail.com>
2020-08-15 14:04:37 -0500

    adding a page controller

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  //static targets = ['name']
+
+  connect() {
+    //console.log("Hello, Stimulus!", this.element)
+    console.log("Hello, Stimulus!")
+  }
+
+}
